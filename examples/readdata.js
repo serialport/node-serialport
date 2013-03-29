c33eac45527f9238206d99ea8a1390de7097916a
Mike OShea <oshea00@gmail.com>
2013-03-28 18:11:53 -0700

    Fixed delimiter choice

--- examples/readdata.js
+++ examples/readdata.js
@@ -7,25 +7,13 @@ var com = require("serialport");
 
 var serialPort = new com.SerialPort("/dev/cu.usbmodemfd121", {
     baudrate: 9600,
-    parser: com.parsers.readline('\r')
+    parser: com.parsers.readline('\r\n')
   });
 
 serialPort.on('open',function() {
   console.log('Port open');
 });
 
-////////////////////////////////////////////////////////
-// Strangely enough, the readLine parser, although it //
-// correctly detects the delimiter '\r', it leaves it //
-// in the buffer. Here's my crude attempt to strip it //
-// out. Yes, I tried replace(/(\r)/g,'') but didn't   //
-// work.                                              //
-////////////////////////////////////////////////////////
 serialPort.on('data', function(data) {
-  var buffer = "";
-  for (var i=0;i<data.length;i++) {
-      if (data.charCodeAt(i)!=10)
-          buffer+=data[i];
-  }
-  console.log(buffer);
+  console.log(data);
 });
