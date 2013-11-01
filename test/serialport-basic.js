c9495a713c3db0aba972fb727d219648e68b71c6
Francis Gulotta <wizard@roborooter.com>
2013-11-01 00:10:39 -0400

    Move the read test into serialport-basic. Make use of sinon sandboxes, refactor the mock serial-port-binding into linux hardware for "remote control"

--- test/serialport-basic.js
+++ test/serialport-basic.js
@@ -1,49 +1,52 @@
 "use strict";
 
-var serialPort = require('../serialport');
 var sinon = require("sinon");
 var chai = require('chai');
-var mockBinding = require('../test_mocks/serial-port-binding');
-
 var expect = chai.expect;
-var SerialPort = serialPort.SerialPort;
-serialPort.SerialPortBinding = mockBinding;
+
+var MockedSerialPort = require('../test_mocks/linux-hardware');
+var SerialPort = MockedSerialPort.SerialPort;
+var hardware = MockedSerialPort.hardware;
+
+// Create a port for fun and profit
+hardware.createPort('/dev/exists');
 
 describe('SerialPort', function () {
+  var sandbox;
+
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create();
+  });
+
+  afterEach(function () {
+    sandbox.restore();
+  });
 
   describe('Constructor', function () {
     it("opens the port immediately", function (done) {
-      var port = new SerialPort('/dev/fun', function (err) {
+      var port = new SerialPort('/dev/exists', function (err) {
         expect(err).to.not.be.ok;
         done();
       });
     });
 
-
     it('emits an error on the factory when erroring without a callback', function (done) {
-      // setup the bindings to report an error during open
-      var stubOpen = sinon.stub(mockBinding, 'open', function (path, opt, cb) {
-        cb('fakeErrr!');
-      });
-
       // finish the test on error
-      serialPort.once('error', function (err) {
+      MockedSerialPort.once('error', function (err) {
         chai.assert.isDefined(err, "didn't get an error");
-        stubOpen.restore();
         done();
       });
 
-      var port = new SerialPort('johnJacobJingleheimerSchmidt');
+      var port = new SerialPort('/dev/johnJacobJingleheimerSchmidt');
     });
 
     it('errors with invalid databits', function (done) {
-
       var errorCallback = function (err) {
         chai.assert.isDefined(err, 'err is not defined');
         done();
       };
 
-      var port = new SerialPort('johnJacobJingleheimerSchmidt', { databits : 19 }, false, errorCallback);
+      var port = new SerialPort('/dev/exists', { databits : 19 }, false, errorCallback);
     });
 
   });
@@ -51,31 +54,39 @@ describe('SerialPort', function () {
   describe('#open', function () {
 
     it('passes the port to the bindings', function (done) {
-      var openSpy = sinon.spy(mockBinding, 'open');
-      var port = new SerialPort('/dev/happyPort', {}, false);
+      var openSpy = sandbox.spy(MockedSerialPort.SerialPortBinding, 'open');
+      var port = new SerialPort('/dev/exists', {}, false);
       port.open(function (err) {
         expect(err).to.not.be.ok;
-        expect(openSpy.calledWith('/dev/happyPort'));
-        openSpy.restore();
+        expect(openSpy.calledWith('/dev/exists'));
         done();
       });
-
     });
 
     it('calls back an error when opening an invalid port', function (done) {
-      var stubOpen = sinon.stub(mockBinding, 'open', function (path, opt, cb) {
-        cb('fakeErrr!');
-      });
 
-      var port = new SerialPort('johnJacobJingleheimerSchmidt', {}, false);
+      var port = new SerialPort('/dev/unhappy', {}, false);
       port.open(function (err) {
         expect(err).to.be.ok;
         done();
       });
-
-      stubOpen.restore();
     });
 
+    it("emits data after being reopened", function (done) {
+      hardware.createPort('/dev/fun');
+
+      var data = new Buffer("Howdy!");
+      var port = new SerialPort('/dev/fun', function () {
+        port.close();
+        port.open(function () {
+          port.once('data', function (res) {
+            expect(res).to.eql(data);
+            done();
+          });
+          hardware.emitData('/dev/fun', data);
+        });
+      });
+    });
 
   });
 
