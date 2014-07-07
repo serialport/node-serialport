8d2cabda6ee8336eb3cc91f390d6ab3f27b5f3e6
Chris Williams <chris@iterativedesigns.com>
2014-07-07 11:34:21 -0400

    Update.

--- serialport.js
+++ serialport.js
@@ -263,11 +263,15 @@ function SerialPortFactory() {
       function afterRead(err, bytesRead, readPool, bytesRequested) {
         self.reading = false;
         if (err) {
+
+          console.log("send to chris --");
+          console.log("|",err.code,"|");
+          console.log("|",err.errno,"|");
           if (err.code && err.code === 'EAGAIN') {
             if (self.fd >= 0) {
               self.serialPoller.start();
             }
-          } else if (err.code && (err.code === 'ENXIO' || err.code === 'UNKNOWN')) {    // handle edge case were mac/unix doesn't clearly know the error.
+          } else if (err.code && (err.code === 'ENXIO' || (err.errno===-1 || err.code === 'UNKNOWN'))) {    // handle edge case were mac/unix doesn't clearly know the error.
             self.disconnected();
           } else {
             self.fd = null;
@@ -275,33 +279,34 @@ function SerialPortFactory() {
             self.emit('error', err);
             self.readable = false;
           }
-        }
-
-        // Since we will often not read the number of bytes requested,
-        // let's mark the ones we didn't need as available again.
-        self.pool.used -= bytesRequested - bytesRead;
-
-        if (bytesRead === 0) {
-          if (self.fd >= 0) {
-            self.serialPoller.start();
-          }
         } else {
-          var b = self.pool.slice(start, start + bytesRead);
+          // Since we will often not read the number of bytes requested,
+          // let's mark the ones we didn't need as available again.
+          self.pool.used -= bytesRequested - bytesRead;
 
-          // do not emit events if the stream is paused
-          if (self.paused) {
-            self.buffer = Buffer.concat([self.buffer, b]);
-            return;
+          if (bytesRead === 0) {
+            if (self.fd >= 0) {
+              self.serialPoller.start();
+            }
           } else {
-            self._emitData(b);
-          }
+            var b = self.pool.slice(start, start + bytesRead);
 
-          // do not emit events anymore after we declared the stream unreadable
-          if (!self.readable) {
-            return;
+            // do not emit events if the stream is paused
+            if (self.paused) {
+              self.buffer = Buffer.concat([self.buffer, b]);
+              return;
+            } else {
+              self._emitData(b);
+            }
+
+            // do not emit events anymore after we declared the stream unreadable
+            if (!self.readable) {
+              return;
+            }
+            self._read();
           }
-          self._read();
         }
+
       }
 
       fs.read(self.fd, self.pool, self.pool.used, toRead, null, function (err, bytesRead) {
