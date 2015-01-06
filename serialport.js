ee743a8201be206f8bd9ab2afc049a0350db4a89
Joshua Perry <josh@6bit.com>
2015-01-06 13:21:46 -0700

    Initial read/write tests and impl

--- serialport.js
+++ serialport.js
@@ -34,7 +34,7 @@ var SETS = ['rts', 'cts', 'dtr', 'dts'];
 
 // The default options, can be overwritten in the 'SerialPort' constructor
 var defaultOptions = {
-  baudrate: 9600,
+  baudRate: 9600,
   parity: 'none',
   rtscts: false,
   xon: false,
@@ -44,8 +44,9 @@ var defaultOptions = {
   cts: false,
   dtr: false,
   dts: false,
-  databits: 8,
-  stopbits: 1,
+  dataBits: 8,
+  stopBits: 1,
+  bufferSize: 255,
   platformOptions: {
     vmin: 1,
     vtime: 0
@@ -80,14 +81,20 @@ function SerialPort(options) {
 util.inherits(SerialPort, stream.Duplex);
 exports.SerialPort = SerialPort;
 
+SerialPort.prototype.read = function(n) {
+  this.read = stream.Readable.prototype.read;
+  return this.read(n);
+};
+
 // Called by the Duplex Readable super class whenever data should
 // be read from the transport
 //
+// Should `push(data)` where `data.length <= n`
+//
 // This function MUST NOT be called directly.
 // 
-// Should `push(data)` where `data.length <= n`
 SerialPort.prototype._read = function(n) {
-  if(this._connecting || !this.isOpen()) {
+  if(!this.isOpen()) {
     debug('queue _read after open');
     // If we're not open, start reading once we are
     // Readable logic will not call this again until previous read is fulfilled
@@ -107,19 +114,23 @@ SerialPort.prototype._write = function (chunk, encoding, callback) {
   // If we aren't open yet, complete this write later
   // The Writable logic will buffer up any more writes while
   // waiting for this one to be done.
-  if(this._connecting) {
+  if(this._opening) {
     this.once('open', function() {
       debug('queue _write after open');
-      this._write.apply(this, arguments);
+      this._write(chunk, encoding, callback);
     });
   } else {
-    debug('_write');
+    debug('_write', this.fd, this.isOpen(), chunk, encoding, !!callback);
 
     if (!this.isOpen()) {
-      throw new Error('The SerialPort must be open to write data.');
+      return callback(new Error('The SerialPort must be open to write data.'));
     }
 
-    SerialPortBinding.write(this.fd, chunk, callback);
+    SerialPortBinding.write(this.fd, chunk, function(err, bytesWrote) {
+      debug('hardware write done', err, bytesWrote);
+
+      callback(err);
+    });
   }
 };
 
@@ -181,9 +192,8 @@ SerialPort.prototype.open = function(options, cb) {
   this._opening = true;
   debug('opening port');
   SerialPortBinding.open(this.options.comname, this.options, function (err, fd) {
-    this._opening = false;
-
     if (err) {
+      this._opening = false;
       debug('native open fail');
       // When opened on the same tick as creation
       // event handlers at call-site will not be attached yet
@@ -201,11 +211,14 @@ SerialPort.prototype.open = function(options, cb) {
       }
 
       this._source.ondata = function(buf) {
+        debug('ondata');
         if(!this.push(buf)) {
+          debug('stream asked us to stop reading');
           this._source.readStop();
         }
       }.bind(this);
 
+      this._opening = false;
       this.fd = fd;
       this.emit('open');
     }
@@ -216,6 +229,7 @@ SerialPort.prototype.isOpen = function() {
   return !!this.fd;
 };
 
+/*
 SerialPort.prototype.disconnected = function (err) {
   // send notification of disconnect
   if (this.options.disconnectedcallback) {
@@ -276,7 +290,7 @@ SerialPort.prototype.end = SerialPort.prototype.close = function (callback) {
   // Stop polling before closing the port.
   if (!isWindows) {
     self.readable = false;
-    self.serialPoller.close();
+    //self.serialPoller.close();
   }
 
   try {
@@ -392,77 +406,21 @@ SerialPort.prototype.drain = function (callback) {
     }
   }
 };
-
-function BaseSource() {
-  this.buffer = new Buffer(16384);
-  this.totalRead = 0;
-  this.totalSent = 0;
-  this.reading = false;
-}
-
-/**
- * Ensures the buffer isn't full
- */
-BaseSource.prototype._ensureBuffer = function() {
-  debug('buffer freespace', this.buffer.length - this.totalRead);
-  // Cycle the buffer if it fills up
-  if(this.totalRead === this.buffer.length) {
-    debug('buffer full');
-    // If the buffer is more than half-full of unsent data, double the current buffer
-    // otherwise keep the same size
-    var watermark = this.totalRead - this.totalSent >= this.buffer.length / 2 ? this.buffer.length * 2 : this.buffer.length;
-    var oldbuf = this.buffer;
-    this.buffer = new Buffer(watermark);
-    debug('new buffer length', watermark);
-
-    // If we have unsent data, copy it from the old buffer
-    if(this.totalSent < this.totalRead) {
-      debug('copying leftover data to new buffer');
-      oldbuf.copy(this.buffer, this.totalSent, this.totalSent + this.totalRead);
-      this.totalRead = this.totalRead - this.totalSent;
-      this.totalSent = 0;
-    } else {
-      this.totalRead = 0;
-      this.totalSent = 0;
-    }
-  }
-};
+*/
 
 /**
  * Dummy source object for windows
  */
 function WindowsSource(options) {
-  BaseSource.call(this);
-
   // The windows native code uses this to send serial
   // data whenever it arrives
-  options.dataCallback = this._dataCallback;
-}
-util.inherits(WindowsSource, BaseSource);
-
-/**
- * Callback to send to the native code to get stream data
- */
-WindowsSource.prototype._dataCallback = function(data) {
-  if(this.ondata && this.reading) {
-    // If we're reading, send the data
-    // Send any unsent buffered data first
-    if(this.totalRead > this.totalSent) {
-      var buffered = this.buffer.slice(this.totalSent, this.totalRead);
-      this.ondata(Buffer.concat([buffered, data]));
-      this.totalSent = this.totalRead;
-    } else {
-      // If we're reading, no sense in creating another buffer
+  options.dataCallback = function(data) {
+    // No buffering here, the readable superclass should handle that
+    if(this.ondata) {
       this.ondata(data);
     }
-  } else {
-    // If we're not reading, buffer the data
-    data.copy(this.buffer, data.length);
-    this.totalRead += data.length;
-  }
-  
-  this._ensureBuffer();
-};
+  }.bind(this);
+}
 
 WindowsSource.prototype.startRead = function() {
   this.reading = true;
@@ -476,9 +434,11 @@ WindowsSource.prototype.endRead = function() {
  * Object that represents a source of data coming from a Unix file descriptor
  */
 function UnixSource(fd) {
-  BaseSource.call(this);
-
   this.fd = fd;
+  this.buffer = new Buffer(16384);
+  this.totalRead = 0;
+  this.totalSent = 0;
+  this.reading = false;
 
   debug('creating serial poller');
   this.serialPoller = new SerialPortBinding.SerialportPoller(fd, function (err) {
@@ -486,16 +446,17 @@ function UnixSource(fd) {
       debug('serial poller err', err);
     } else {
       debug('serial poller data available');
-      this.dataAvailable();
+      this._dataAvailable();
     }
   }.bind(this));
 }
-util.inherits(UnixSource, BaseSource);
 
 /**
  * Callback to read data when it is available from the serial port
+ * 
+ * We use a data buffer here so that we're not wasting buffer allocations for small reads
  */
-UnixSource.prototype.dataAvailable = function() {
+UnixSource.prototype._dataAvailable = function() {
     debug('data available');
     fs.read(this.fd, this.buffer, this.totalRead, this.buffer.length - this.totalRead, null, function (err, bytesRead) {
       if(err) {
@@ -532,6 +493,34 @@ UnixSource.prototype.readStart = function() {
  */
 UnixSource.prototype.readStop = function() {
   this.reading = false;
+};
+
+/**
+ * Ensures the buffer isn't full
+ */
+UnixSource.prototype._ensureBuffer = function() {
+  debug('buffer freespace', this.buffer.length - this.totalRead);
+  // Cycle the buffer if it fills up
+  if(this.totalRead === this.buffer.length) {
+    debug('buffer full');
+    // If the buffer is more than half-full of unsent data, double the current buffer
+    // otherwise keep the same size
+    var watermark = this.totalRead - this.totalSent >= this.buffer.length / 2 ? this.buffer.length * 2 : this.buffer.length;
+    var oldbuf = this.buffer;
+    this.buffer = new Buffer(watermark);
+    debug('new buffer length', watermark);
+
+    // If we have unsent data, copy it from the old buffer
+    if(this.totalSent < this.totalRead) {
+      debug('copying leftover data to new buffer');
+      oldbuf.copy(this.buffer, this.totalSent, this.totalSent + this.totalRead);
+      this.totalRead = this.totalRead - this.totalSent;
+      this.totalSent = 0;
+    } else {
+      this.totalRead = 0;
+      this.totalSent = 0;
+    }
+  }
 };
 
 
--- test/serialport-basic.js
+++ test/serialport-basic.js
@@ -4,6 +4,7 @@ var serialModule = require('../test_mocks/linux-hardware'),
     SerialPort = serialModule.SerialPort,
     hardware = serialModule.hardware,
     stream = require('readable-stream'),
+    through2 = require('through2'),
     _ = require('lodash'),
     B = require('bluebird');
 
@@ -22,12 +23,13 @@ function assertNextTick(act, assert) {
 describe('SerialPort', function () {
   var sandbox;
   var existPath = '/dev/exists';
+  var mockPort;
 
   beforeEach(function () {
     sandbox = sinon.sandbox.create();
 
     // Create a port for fun and profit
-    hardware.createPort(existPath);
+    mockPort = hardware.createPort(existPath);
   });
 
   afterEach(function () {
@@ -228,6 +230,169 @@ describe('SerialPort', function () {
     });
     */
 
+  });
+
+  describe('read', function() {
+    var port;
+    beforeEach(function() {
+      port = new SerialPort();
+    });
+
+    describe('when not open', function() {
+      it('should postpone requested read before open', function() {
+        // Signal read interest
+        port.once('readable', function() { });
+
+        port.open({ comname: existPath });
+
+        port._source.reading.should.be.true;
+      });
+
+      it('should postpone requested read while opening', function() {
+        // Prevent the mock hardware from completing the open
+        sandbox.stub(serialModule.SerialPortBinding, 'open');
+        port.open({ comname: existPath });
+
+        // Signal read interest
+        port.once('readable', function() { });
+
+        // Execute `open` callback
+        var args = serialModule.SerialPortBinding.open.firstCall.args;
+        args[2](null, args[0]);
+
+        port._source.reading.should.be.true;
+      });
+    });
+
+    describe('when open', function() {
+      beforeEach(function() {
+        port.open({ comname: existPath });
+      });
+
+      it('should not start unrequested reading', function() {
+        port._source.reading.should.be.false;
+      });
+
+      it('should read when requested', function() {
+        var data = new Buffer('blah', 'ascii');
+
+        port.once('readable', function() {
+          expect(port.read()).to.deep.equal(data);
+        });
+
+        hardware.emitData(existPath, data);
+      });
+    });
+  });
+
+  describe('write', function() {
+    var port;
+    beforeEach(function() {
+      port = new SerialPort();
+    });
+
+    describe('when not open', function() {
+      it('should emit error', function() {
+        var spy = sinon.spy();
+        port.on('error', spy);
+
+        return assertNextTick(function() {
+          port.write('blah', 'ascii');
+        }, function() {
+          spy.should.have.been.calledOnce;
+          spy.should.have.been.calledWith(sinon.match.instanceOf(Error));
+          spy.should.have.been.calledWith(sinon.match.has('message', sinon.match(/must be open/)));
+        });
+      });
+
+      it('should callback with error', function() {
+        // Suppress emitter throws
+        port.on('error', function() {});
+
+        var spy = sinon.spy();
+
+        return assertNextTick(function() {
+          port.write('blah', 'ascii', spy);
+        }, function() {
+          spy.should.have.been.calledOnce;
+          spy.should.have.been.calledWith(sinon.match.instanceOf(Error));
+          spy.should.have.been.calledWith(sinon.match.has('message', sinon.match(/must be open/)));
+        });
+      });
+
+      it('should postpone write while opening', function() {
+        var write = sandbox.stub(serialModule.SerialPortBinding, 'write');
+        // Prevent the mock hardware from completing the open
+        sandbox.stub(serialModule.SerialPortBinding, 'open');
+
+        port.open({ comname: existPath });
+
+        // Do write while opening
+        var data = new Buffer('blah', 'ascii');
+        var callback = function() {};
+        port.write(data, callback);
+
+        // Make sure no write was done to the hardware
+        serialModule.SerialPortBinding.write.should.not.have.been.called;
+
+        // Complete the mock hardware `open`
+        var args = serialModule.SerialPortBinding.open.firstCall.args;
+        args[2](null, args[0]);
+
+        // See that a queued write was done now that opening is complete
+        write.should.have.been.calledOnce;
+        write.should.have.been.calledWith(sinon.match(existPath), sinon.match.same(data), sinon.match.func);
+      });
+    });
+
+    describe('when open', function() {
+      beforeEach(function() {
+        port.open({ comname: existPath });
+      });
+
+      it('should write data', function() {
+        var write = sandbox.stub(serialModule.SerialPortBinding, 'write');
+        var data = new Buffer('blah', 'ascii');
+        var callback = sinon.spy();
+
+        port.write(data, callback);
+
+        write.should.have.been.calledOnce;
+        write.should.have.been.calledWith(sinon.match(existPath), sinon.match.same(data), sinon.match.func);
+      });
+
+      it('should callback when write completes', function() {
+        var data = new Buffer('blah', 'ascii');
+        var callback = sinon.spy();
+        var write = sandbox.stub(serialModule.SerialPortBinding, 'write');
+        write.yields(null, data.length);
+
+        return assertNextTick(function() {
+          port.write(data, callback);
+        }, function() {
+          expect(write, 'Hardware write not called').to.have.been.called;
+          expect(callback, 'Done callback never called').to.have.been.called;
+        });
+      });
+
+      it('should write stream', function() {
+        var data = new Buffer('blah', 'ascii');
+        var callback = sinon.spy();
+        var write = sandbox.stub(serialModule.SerialPortBinding, 'write');
+        write.yields(null, data.length);
+
+        var input = new stream.Readable();
+        input.push(data);
+        input.push(null);
+
+        return assertNextTick(function() {
+          input.pipe(port);
+        }, function() {
+          write.should.have.been.calledOnce;
+          write.should.have.been.calledWith(sinon.match(existPath), sinon.match.same(data), sinon.match.func);
+        });
+      });
+    });
   });
 
   /*
