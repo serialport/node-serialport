6f453b60ac22909334dd128832d7b5eceecbbca6
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-03-01 22:04:40 -0700

    whitespace

--- examples/break.js
+++ examples/break.js
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////
 // Use the cool library                               //
 // git://github.com/voodootikigod/node-serialport.git //
-// to send break condition.							  //
+// to send break condition.                           //
 ////////////////////////////////////////////////////////               
 var com = require("serialport");
 
@@ -20,20 +20,20 @@ serialPort.on('data', function(data) {
 
 function asserting() {
   console.log('asserting');
-	serialPort.set({brk:true}, function(err, something) {
-	  console.log('asserted');
-		setTimeout(clear, 5000);
-	});
+  serialPort.set({brk:true}, function(err, something) {
+    console.log('asserted');
+    setTimeout(clear, 5000);
+  });
 }
 
 function clear() {
-	console.log('clearing');
-	serialPort.set({brk:false}, function(err, something) {
-	  console.log('clear');
-		setTimeout(done, 5000);
-	});
+  console.log('clearing');
+  serialPort.set({brk:false}, function(err, something) {
+    console.log('clear');
+    setTimeout(done, 5000);
+  });
 }
 
 function done() {
-	console.log("done breaking");
+  console.log("done breaking");
 }
