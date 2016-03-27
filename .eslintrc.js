c2c424c620f012230832d7e344adee0c380c1bb4
Francis Gulotta <wizard@roborooter.com>
2016-03-27 13:48:18 -0400

    Switch to eslint and fixup a lot of listing issues.
    There are not a ton of open PRs that would be effected by this, I think it's a good time to go ahead and merge this right away.

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: "standard",
+  env: {
+    node: true,
+    mocha: true,
+  },
+  plugins: [
+      "standard"
+  ],
+  parserOptions: {
+    sourceType: "script"
+  },
+  rules: {
+    "eqeqeq": 2,
+    "no-use-before-define": 2,
+    "no-caller": 2,
+    "max-depth": 2,
+    "complexity": [2, 32],
+    "max-statements": [2, 58],
+    "no-else-return": 2, // maybe
+    "wrap-iife": [2, "inside"],
+    "new-cap": 2,
+    "strict": 2,
+    "curly": 2,
+    "quotes": [2, "single", "avoid-escape"],
+    "brace-style": 2,
+    "no-unused-vars": [2, { "args": "after-used" }],
+    "space-before-function-paren": 0,
+    "semi": [2, "always", {
+      "omitLastInOneLineBlock": true
+    }]
+  }
+};
--- parsers.js
+++ parsers.js
@@ -1,4 +1,3 @@
-/*jslint node: true */
 'use strict';
 
 // Copyright 2011 Chris Williams <chris@iterativedesigns.com>
@@ -8,11 +7,11 @@ module.exports = {
     emitter.emit('data', buffer);
   },
 
-  //encoding: ascii utf8 utf16le ucs2 base64 binary hex
-  //More: http://nodejs.org/api/buffer.html#buffer_buffer
+  // encoding: ascii utf8 utf16le ucs2 base64 binary hex
+  // More: http://nodejs.org/api/buffer.html#buffer_buffer
   readline: function (delimiter, encoding) {
-    if (typeof delimiter === 'undefined' || delimiter === null) { delimiter = '\r'; }
-    if (typeof encoding  === 'undefined' || encoding  === null) { encoding  = 'utf8'; }
+    if (typeof delimiter === 'undefined' || delimiter === null) { delimiter = '\r' }
+    if (typeof encoding === 'undefined' || encoding === null) { encoding = 'utf8' }
     // Delimiter buffer saved in closure
     var data = '';
     return function (emitter, buffer) {
@@ -30,20 +29,20 @@ module.exports = {
   // Emit a data event every `length` bytes
   byteLength: function(length) {
     var data = new Buffer(0);
-    return function(emitter, buffer){
+    return function(emitter, buffer) {
       data = Buffer.concat([data, buffer]);
       while (data.length >= length) {
-        var out = data.slice(0,length);
+        var out = data.slice(0, length);
         data = data.slice(length);
         emitter.emit('data', out);
       }
     };
   },
 
-  //Emit a data event each time a byte sequence (delimiter is an array of byte) is found
-  //Sample usage : byteDelimiter([10, 13])
+  // Emit a data event each time a byte sequence (delimiter is an array of byte) is found
+  // Sample usage : byteDelimiter([10, 13])
   byteDelimiter: function (delimiter) {
-    if( Object.prototype.toString.call( delimiter ) !== '[object Array]' ){
+    if (Object.prototype.toString.call(delimiter) !== '[object Array]') {
       delimiter = [ delimiter ];
     }
     var buf = [];
@@ -51,10 +50,10 @@ module.exports = {
     return function (emitter, buffer) {
       for (var i = 0; i < buffer.length; i++) {
         buf[buf.length] = buffer[i];
-        if(buf[buf.length-1] === delimiter[nextDelimIndex]) {
+        if (buf[buf.length - 1] === delimiter[nextDelimIndex]) {
           nextDelimIndex++;
         }
-        if(nextDelimIndex === delimiter.length) {
+        if (nextDelimIndex === delimiter.length) {
           emitter.emit('data', buf);
           buf = [];
           nextDelimIndex = 0;
