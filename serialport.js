c93fb3b81f65c7dd659ced4d69c820667d34b952
Francis Gulotta <wizard@roborooter.com>
2016-03-28 00:02:07 -0400

    Move listUnix to its own module
     - Refactor to fix bugs 
     - Remove the path behind `udevadm` as it’s not always in the same place but it’s so far it is always in the path
     - Tests listUnix, (`pnpId` assumes a lot about `udevadm` =( )
     - Adds `ttyMDF` to the list of serial ports for “High Speed UART device of Intel Medfield platform”
     - turn down the verboseness on appveyor nam installs
     - upgrade sandboxed-module for windows support
    
    Fixes #650,  #651, #634, #697 and #621

--- serialport.js
+++ serialport.js
@@ -12,15 +12,12 @@ var path = require('path');
 var PACKAGE_JSON = path.join(__dirname, 'package.json');
 var binding_path = binary.find(path.resolve(PACKAGE_JSON));
 var SerialPortBinding = require(binding_path);
-
-var parsers = require('./parsers');
+var parsers = require('./lib/parsers');
+var listUnix = require('./lib/list-unix');
 var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 var fs = require('fs');
 var stream = require('stream');
-var async = require('async');
-var exec = require('child_process').exec;
-
 function SerialPortFactory(_spfOptions) {
   _spfOptions = _spfOptions || {};
 
@@ -502,82 +499,6 @@ function SerialPortFactory(_spfOptions) {
     }
   };
 
-  function listUnix(callback) {
-    function udev_parser(udev_output, callback) {
-      function udev_output_to_json(output) {
-        var result = {};
-        var lines = output.split('\n');
-        for (var i = 0; i < lines.length; i++) {
-          var line = lines[i].trim();
-          if (line !== '') {
-            var line_parts = lines[i].split('=');
-            result[line_parts[0].trim()] = line_parts[1].trim();
-          }
-        }
-        return result;
-      }
-      var as_json = udev_output_to_json(udev_output);
-
-      var pnpId;
-      if (as_json.DEVLINKS) {
-        pnpId = as_json.DEVLINKS.split(' ')[0];
-        pnpId = pnpId.substring(pnpId.lastIndexOf('/') + 1);
-      }
-      var port = {
-        comName: as_json.DEVNAME,
-        manufacturer: as_json.ID_VENDOR,
-        serialNumber: as_json.ID_SERIAL,
-        pnpId: pnpId,
-        vendorId: '0x' + as_json.ID_VENDOR_ID,
-        productId: '0x' + as_json.ID_MODEL_ID
-      };
-
-      callback(null, port);
-    }
-
-    // var dirName = (spfOptions.queryPortsByPath ? '/dev/serial/by-path' : '/dev/serial/by-id');
-    var dirName = '/dev';
-
-    fs.readdir(dirName, function (err, files) {
-      if (err) {
-        // if this directory is not found this could just be because it's not plugged in
-        if (err.errno === 34) {
-          return callback(null, []);
-        }
-
-        if (callback) {
-          callback(err);
-        } else {
-          factory.emit('error', err);
-        }
-        return;
-      }
-
-      // get only serial port  names
-      for (var i = files.length - 1; i >= 0; i--) {
-        if ((files[i].indexOf('ttyS') === -1 && files[i].indexOf('ttyACM') === -1 && files[i].indexOf('ttyUSB') === -1 && files[i].indexOf('ttyAMA') === -1) || !fs.statSync(path.join(dirName, files[i])).isCharacterDevice()) {
-          files.splice(i, 1);
-        }
-      }
-
-      async.map(files, function (file, callback) {
-        var fileName = path.join(dirName, file);
-        exec('/sbin/udevadm info --query=property -p $(/sbin/udevadm info -q path -n ' + fileName + ')', function (err, stdout) {
-          if (err) {
-            if (callback) {
-              callback(err);
-            } else {
-              factory.emit('error', err);
-            }
-            return;
-          }
-
-          udev_parser(stdout, callback);
-        });
-      }, callback);
-    });
-  }
-
   SerialPort.prototype.flush = function (callback) {
     var self = this;
     var fd = self.fd;
@@ -668,18 +589,22 @@ function SerialPortFactory(_spfOptions) {
   };
 
   factory.SerialPort = SerialPort;
-  factory.parsers = parsers;
-  factory.SerialPortBinding = SerialPortBinding;
-
-  if (process.platform === 'win32') {
-    factory.list = SerialPortBinding.list;
-  } else if (process.platform === 'darwin') {
-    factory.list = SerialPortBinding.list;
-  } else {
-    factory.list = listUnix;
-  }
 }
 
 util.inherits(SerialPortFactory, EventEmitter);
+
+SerialPortFactory.prototype.parsers = parsers;
+SerialPortFactory.prototype.SerialPortBinding = SerialPortBinding;
+
+if (process.platform === 'win32' || process.platform === 'darwin') {
+  SerialPortFactory.prototype.list = SerialPortBinding.list;
+} else {
+  SerialPortFactory.prototype.list = function(callback) {
+    callback = callback || function(err) {
+      if (err) { this.emit('error', err) }
+    }.bind(this);
+    return listUnix(callback);
+  };
+}
 
 module.exports = new SerialPortFactory();
