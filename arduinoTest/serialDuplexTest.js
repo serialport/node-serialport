25d27e5a9891b26ffd8f33d5ec928c64bb20e451
tigoe <tom.igoe@gmail.com>
2016-03-29 06:37:35 -0400

    duly linted.

--- arduinoTest/serialDuplexTest.js
+++ arduinoTest/serialDuplexTest.js
@@ -7,47 +7,45 @@ To be used in conjunction with the Arduino sketch called ArduinoEcho.ino
 'use strict';
 
 // serial port initialization:
-var serialport = require('../serialport'),		// include the serialport library
-SerialPort  = serialport.SerialPort,			// make a local instance of serial
-portName = process.argv[2];								// get the port name from the command line
-
-var output = 32;													// ASCII space; lowest printable character
-var byteCount = 0;												// number of bytes read
-// open the serial port:
-var myPort = new SerialPort(portName);
+var serialport = require('../serialport');     // include the serialport library
+var SerialPort = serialport.SerialPort;         // make a local instance of serial
+var portName = process.argv[2];                 // get the port name from the command line
+var myPort = new SerialPort(portName);          // open the serial port:
+var output = 32;                                // ASCII space; lowest printable character
+var byteCount = 0;                              // number of bytes read
 
 function openPort() {
-	console.log('port open');
-	console.log('baud rate: ' + myPort.options.baudRate);
-	var outString = String.fromCharCode(output);
-	console.log('String is: ' + outString);
-	myPort.write(outString);
+  console.log('port open');
+  console.log('baud rate: ' + myPort.options.baudRate);
+  var outString = String.fromCharCode(output);
+  console.log('String is: ' + outString);
+  myPort.write(outString);
 }
 
 function receiveData(data) {
-	if (output <= 126) {				// highest printable character: ASCII ~
-		output++;
-	} else {
-		output = 32;							// lowest printable character: space
-	}
-	console.log('received: ' + data);
-	console.log('Byte count: ' + byteCount);
-	byteCount++;
-	var outString = String.fromCharCode(output);
-	myPort.write(outString);
-	console.log('Sent: ' + outString);
+  if (output <= 126) {        // highest printable character: ASCII ~
+    output++;
+  } else {
+    output = 32;              // lowest printable character: space
+  }
+  console.log('received: ' + data);
+  console.log('Byte count: ' + byteCount);
+  byteCount++;
+  var outString = String.fromCharCode(output);
+  myPort.write(outString);
+  console.log('Sent: ' + outString);
 }
 
 function closePort() {
-	console.log('port closed');
+  console.log('port closed');
 }
 
 function serialError(error) {
-	console.log('there was an error with the serial port: ' + error);
-	myPort.close();
+  console.log('there was an error with the serial port: ' + error);
+  myPort.close();
 }
 
-myPort.on('open', openPort);			// called when the serial port opens
-myPort.on('data', receiveData);		// called when data comes in
-myPort.on('close', closePort);		// called when the serial port closes
-myPort.on('error', serialError);	// called when there's an error with the serial port
+myPort.on('open', openPort);      // called when the serial port opens
+myPort.on('data', receiveData);    // called when data comes in
+myPort.on('close', closePort);    // called when the serial port closes
+myPort.on('error', serialError);  // called when there's an error with the serial port
