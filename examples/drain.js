453a47794443880bbd85785b05b8885ff03c5f33
Francis Gulotta <wizard@roborooter.com>
2017-01-03 23:35:20 -0500

    Update the examples for 5x

--- examples/drain.js
+++ examples/drain.js
@@ -3,23 +3,25 @@
 const SerialPort = require('serialport');
 const port = new SerialPort('/dev/cu.Cubelet-RGB');
 
-port.on('open', function() {
-  const largeMessage = new Buffer(1024 * 10).fill('!');
-  console.log('Calling write');
-  port.write(largeMessage, function() {
-    console.log('Write callback returned');
-    // At this point, data may still be buffered and not sent out over the port yet
-    // write function returns asynchronously even on the system level.
-    console.log('Calling drain');
-    port.drain(function() {
-      console.log('Drain callback returned');
-      // Now the data has "left the pipe" (tcdrain[1]/FlushFileBuffers[2] finished blocking).
-      // [1] http://linux.die.net/man/3/tcdrain
-      // [2] http://msdn.microsoft.com/en-us/library/windows/desktop/aa364439(v=vs.85).aportx
-    });
+port.on('open', () => {
+  console.log('port opened');
+});
+
+const largeMessage = new Buffer(1024 * 10).fill('!');
+port.write(largeMessage, () => {
+  console.log('Write callback returned');
+
+  // At this point, data may still be buffered and not sent out over the port yet
+  // write function returns asynchronously even on the system level.
+  console.log('Calling drain');
+  port.drain(() => {
+    console.log('Drain callback returned');
+    // Now the data has "left the pipe" (tcdrain[1]/FlushFileBuffers[2] finished blocking).
+    // [1] http://linux.die.net/man/3/tcdrain
+    // [2] http://msdn.microsoft.com/en-us/library/windows/desktop/aa364439(v=vs.85).aportx
   });
 });
 
-port.on('data', function(data) {
+port.on('data', (data) => {
   console.log('Received: \t', data);
 });
---
+++ examples/open-callback.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Constructor callback example
+const SerialPort = require('serialport');
+const port = new SerialPort('/dev/tty-usbserial1', () => {
+  console.log('Port Opened');
+});
+
+port.write('main screen turn on', (err) => {
+  if (err) { return console.log('Error: ', err.message) }
+  console.log('message written');
+});
