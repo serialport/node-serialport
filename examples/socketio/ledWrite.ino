b8b77e725f8a01feffbdfef9f5d390239c2dbcfc
Jessica Quynh Tran <jessica.quynh.tran@gmail.com>
2017-02-19 19:39:54 -0500

    Add socketio example (#1081)
    * Add socketio example
    * fix json in .eslintrc file

---
+++ examples/socketio/ledWrite.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (Serial.available()) {
+    int byte = Serial.read();
+    digitalWrite(LED_BUILTIN, byte);
+    Serial.print(byte);
+  }
+}
