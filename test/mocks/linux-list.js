8e338c360099e0513ba03a237921d59e497e6f1a
Jesus <akajes@ukr.net>
2017-07-23 17:15:54 +0300

    tests

--- test/mocks/linux-list.js
+++ test/mocks/linux-list.js
@@ -4,42 +4,26 @@
 
 const proxyquire = require('proxyquire');
 
-let mockPorts = {};
-let characterDevice = true;
+let mockPorts;
 let error = false;
 
 proxyquire.noPreserveCache();
 const listLinux = proxyquire('../../lib/bindings/linux-list', {
-  fs: {
-    readdir(path, cb) {
-      if (error) {
-        return process.nextTick(() => cb(new Error('Bad')));
-      }
-      process.nextTick(() => cb(null, Object.keys(mockPorts)));
-    },
-    stat(path, cb) {
-      process.nextTick(() => cb(null, { isCharacterDevice() { return characterDevice } }));
-    }
-  },
-  path: {
-    // needed for testing on windows
-    join() {
-      return Array.prototype.join.call(arguments, '/');
-    }
-  },
   child_process: {
-    exec(cmd, cb) {
-      const port = cmd.split(/\/dev\/(.*)\)/)[1];
-      process.nextTick(() => cb(null, mockPorts[port]));
+    spawn(cmd, cb) {
+      const EventEmitter = require('events');
+      let event = new EventEmitter();
+      const Readable = require('stream').Readable
+      var stream = new Readable;
+      stream.push(mockPorts);
+      stream.push(null);
+      event.stdout = stream;
+      return event;
     }
   }
 });
 
 proxyquire.preserveCache();
-
-listLinux.setCharacterDevice = (isCharacterDevice) => {
-  characterDevice = isCharacterDevice;
-};
 
 listLinux.setPorts = (ports) => {
   mockPorts = ports;
@@ -52,7 +36,6 @@ listLinux.error = (err) => {
 listLinux.reset = () => {
   error = false;
   mockPorts = {};
-  characterDevice = true;
 };
 
 module.exports = listLinux;
