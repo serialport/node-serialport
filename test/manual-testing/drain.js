bbeac32614b517162e25c6648829547c1b278ac3
John Whittington <john.whittington@cruxproductdesign.com>
2017-07-27 12:07:32 +0100

    Use parser not data callback without time and linting

--- test/manual-testing/drain.js
+++ test/manual-testing/drain.js
@@ -25,13 +25,7 @@ serialPort.write(largeMessage, () => {
   console.log('Write callback returned');
 });
 
-// At this point, data may still be buffered in the os or node serialserialPort and not sent
-// out over the serialPort yet. SerialserialPort will wait until any pending writes have completed and then ask
-// the operating system to wait until all data has been written to the file descriptor.
 console.log('Calling drain');
 serialPort.drain(() => {
   console.log('Drain callback returned');
-  // Now the data has "left the pipe" (tcdrain[1]/FlushFileBuffers[2] finished blocking).
-  // [1] http://linux.die.net/man/3/tcdrain
-  // [2] http://msdn.microsoft.com/en-us/library/windows/desktop/aa364439(v=vs.85).aserialPortx
 });
