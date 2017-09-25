e43809c1ac75948e8d474f22ff0bea2ec836bfa2
Frank Lemanschik <frank@dspeed.eu>
2017-09-25 13:00:24 +0100

    Fixed: parser and Tests also updated to use array

--- test/parser-cctalk.js
+++ test/parser-cctalk.js
@@ -29,7 +29,10 @@ describe('CCTalkParser', () => {
     const parser = new CCTalkParser();
     const spy = sinon.spy();
     parser.on('data', spy);
-    parser.write(Buffer.from([2, 2, 1, 254, 1, 1, 217, 2, 0, 1, 254, 217]));
+    parser.write(Buffer.from([2, 2, 1]));
+    parser.write(Buffer.from([254, 1, 1]));
+    parser.write(Buffer.from([217, 2]));
+    parser.write(Buffer.from([0, 1, 254, 217]));
     assert.equal(spy.callCount, 2);
     assert.deepEqual(spy.getCall(0).args[0], Buffer.from([2, 2, 1, 254, 1, 1, 217]));
     assert.deepEqual(spy.getCall(1).args[0], Buffer.from([2, 0, 1, 254, 217]));
