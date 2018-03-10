94ede443c22ce75f9df2ceeaaebf676d96c3f784
Francis Gulotta <wizard@roborooter.com>
2018-03-10 14:55:27 -0500

    feat: generators for new packages (#4)

---
+++ packages/serialport-util/.generators/README.md
@@ -0,0 +1,11 @@
+# {{pascalCase name}}
+
+This stream does some neat stuff.
+
+This is why you'd use it.
+
+This is how you use it.
+```js
+const {{camelCase name}} = new {{pascalCase name}}()
+
+```
---
+++ packages/serialport-util/.generators/{{dashCase name}}-test.js
@@ -0,0 +1,13 @@
+'use strict'
+/* eslint-disable no-new */
+
+const Buffer = require('safe-buffer').Buffer
+const sinon = require('sinon')
+const {{pascalCase name}} = require('./{{dashCase name}}')
+
+describe('{{pascalCase name}}', () => {
+  it('constructs', () => {
+    new {{pascalCase name}}()
+  })
+  it('needs more testing')
+})
