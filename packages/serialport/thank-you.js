03972672a8a74f145068a0c9b3e2268cd9c8bcba
Francis Gulotta <wizard@roborooter.com>
2021-02-21 22:28:06 -0500

    chore: cleanup package.json's
    - Remove the post install in serialport and switch to the funding property
    - Update the engines field as we don't support node 8 anymore, only node >= 10.0.0

--- packages/serialport/thank-you.js
+++
@@ -1,3 +0,0 @@
-const message =
-  '\u001b[96m\u001b[1mThank you for using serialport!\u001b[96m\u001b[1m\n\u001b[0m\u001b[96mIf you rely on this package, please consider supporting our open collective:\u001b[22m\u001b[39m\n> \u001b[94mhttps://opencollective.com/serialport/donate\u001b[0m\n\n'
-console.log(message)
